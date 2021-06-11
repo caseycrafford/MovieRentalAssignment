3c117a483d7b791d1b8f156ce12138d8ac131009
Malcolm Camelo <217137334@mycput.ac.za>
2021-06-11 22:00:24 +0200

    tests completed for each entity

--- src/test/java/za/ac/cput/Entity/ContactTypeTest.java
+++ src/test/java/za/ac/cput/Entity/ContactTypeTest.java
@@ -1,8 +1,15 @@
 package za.ac.cput.Entity;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import za.ac.cput.entity.ContactType;
 import za.ac.cput.factory.ContactTypeFactory;
+
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 /* ContactTypeTest.java
 Test for the ContactType
  Author: Malcolm Camelo (217137334)
@@ -18,4 +25,35 @@ class ContactTypeTest {
 
 
     }
+    @Test
+    void test2Equal() {
+        ContactType uc = ContactTypeFactory.build("b","c",84385385);
+        ContactType uc2 = ContactTypeFactory.build("a","e",84385385);
+        assertNotEquals(uc, uc2);
+    }
+    @Test
+    void test3Identity()
+    {
+        ContactType uc = ContactTypeFactory.build("b","c",84385385);
+        assertSame(uc,uc);
+    }
+    @Timeout(value = 1, unit = MILLISECONDS)
+    void test4TimeOut()
+    {
+        for (int i =0;i<100;i++)
+        {
+            ContactType uc = ContactTypeFactory.build("b","c",84385385);
+            System.out.println(uc);
+        }
+    }
+
+    @Disabled
+    void test5Disable()
+    {
+        int v =2;
+        int i=1;
+        assertSame(i,v);
+    }
+
+
 }
--- src/test/java/za/ac/cput/Entity/UserContactTest.java
+++ src/test/java/za/ac/cput/Entity/UserContactTest.java
@@ -1,7 +1,13 @@
 package za.ac.cput.Entity;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 import za.ac.cput.entity.UserContact;
 import za.ac.cput.factory.UserContactFactory;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static org.junit.jupiter.api.Assertions.*;
+
 /* UserContactTest.java
  Test for the UserContact
  Author: Malcolm Camelo (217137334)
@@ -19,6 +25,34 @@ class UserContactTest {
 
 
     }
+@Test
+    void test2Equal() {
+    UserContact uc = UserContactFactory.build(165216712, 34934, "021782567");
+    UserContact uc2 = UserContactFactory.build(1652123312, 34934, "021782567");
+    assertNotEquals(uc, uc2);
+}
+@Test
+    void test3Identity()
+{
+    UserContact uc = UserContactFactory.build(165216712, 34934, "021782567");
+    assertSame(uc,uc);
+}
+@Timeout(value = 1, unit = MILLISECONDS)
+    void test4TimeOut()
+{
+for (int i =0;i<100;i++)
+{
+    UserContact uc = UserContactFactory.build(165216712, 34934, "021782567");
+    System.out.println(uc);
+}
+}
 
+@Disabled
+    void test5Disable()
+{
+    int v =2;
+    int i=1;
+    assertSame(i,v);
+}
 
 }
