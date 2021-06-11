eb76e94d195e1e2fff5a360ea125cbe1288c8cc7
YaasinCole <yaasincole1999@gmail.com>
2021-06-11 19:34:03 +0200

    init commit

---
+++ src/main/java/za/ac/cput/Entity/User.java
@@ -0,0 +1,73 @@
+package za.ac.cput.Entity;
+
+/**
+ * Author: Muhammad Yaasin Cole
+ * Student Number: 218340869
+ *
+ */
+public class User {
+
+    private int userId ,addressId , idNumber;
+    private String firstName,lastName;
+
+    private User(userBuilder userBuilder)
+    {
+        this.userId = userBuilder.userId;
+        this.addressId = userBuilder.addressId;
+        this.idNumber = userBuilder.idNumber;
+        this.firstName = userBuilder.firstName;
+        this.lastName = userBuilder.lastName;
+
+
+    }
+    public static class userBuilder
+    {
+        private int userId ,addressId , idNumber;
+        private String firstName,lastName;
+
+        public userBuilder setUserId(int userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        public userBuilder setAddressId(int addressId) {
+            this.addressId = addressId;
+            return this;
+        }
+
+        public userBuilder setIdNumber(int idNumber) {
+            this.idNumber = idNumber;
+            return this;
+        }
+
+        public userBuilder setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public userBuilder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public User build()
+        {
+            return new User(this);
+        }
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", addressId=" + addressId +
+                ", idNumber=" + idNumber +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+
+}
