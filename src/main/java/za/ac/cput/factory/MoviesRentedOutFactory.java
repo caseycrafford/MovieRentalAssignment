fcfbd7d8f0dedf301cdf97a008dacadfd02ffd91
LCK2304 <71738532+LCK2304@users.noreply.github.com>
2021-06-11 23:47:30 +0200

    Lance CP

---
+++ src/main/java/za/ac/cput/factory/MoviesRentedOutFactory.java
@@ -0,0 +1,19 @@
+package za.ac.cput.factory;
+import za.ac.cput.entity.MovieRentedOut;
+import java.util.UUID;
+
+import static javax.swing.text.html.parser.DTDConstants.ID;
+
+
+public class MoviesRentedOutFactory {
+    public static MovieRentedOut build(int movieID, int quantity){
+
+        if (movieID<0 || quantity<0)
+            return null;
+
+        int ID= Integer.parseInt(UUID.randomUUID().toString());
+
+        return new MovieRentedOut.MovieRentedOutBuilder().
+                setMovieID(ID).setQuantity(quantity).build();
+    }
+}
