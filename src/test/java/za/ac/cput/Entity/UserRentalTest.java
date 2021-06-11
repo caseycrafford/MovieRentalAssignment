7ce734eb2f962474c62aad6734419189b69f6a5f
TheMorningStar <216061324@mycput.ac.za>
2021-06-11 23:08:11 +0200

    Added test classes

---
+++ src/test/java/za/ac/cput/Entity/UserRentalTest.java
@@ -0,0 +1,42 @@
+package za.ac.cput.Entity;
+
+/*
+ * Author: Grant Constant
+ * Student number: 216063124
+ */
+
+import org.junit.jupiter.api.*;
+
+public class UserRentalTest {
+
+    @Test
+    void test ()
+    {
+
+    }
+
+    @Test
+    void testEqualty()
+    {
+
+    }
+
+    @Test
+    void testIdentity()
+    {
+
+    }
+
+    @Disabled
+    void disabledTest()
+    {
+
+    }
+
+    @Timeout(10)
+    @Test
+    void timeoutTest()
+    {
+
+    }
+}
