fd9380f6b0836ede7707a1fceae609a1e46e191e
lancekruger <lanceckruger2304@gmail.com>
2021-07-26 22:55:38 +0200

    Lance's push

---
+++ src/main/java/za/ac/cput/repository/NumberOfMoviesRentedRepository.java
@@ -0,0 +1,64 @@
+package za.ac.cput.repository;
+
+import za.ac.cput.entity.NumberOfMoviesRented;
+
+import java.util.*;
+
+public class NumberOfMoviesRentedRepository implements INumberOfMoviesRented {
+        private static NumberOfMoviesRentedRepository repository = null;
+        public Set<NumberOfMoviesRented> numberOfMoviesRentedDB = null;
+
+        public NumberOfMoviesRentedRepository() {
+
+            numberOfMoviesRentedDB = new HashSet<NumberOfMoviesRented>();
+        }
+
+        public static NumberOfMoviesRentedRepository getRepository() {
+            if (repository == null) {
+                repository = new NumberOfMoviesRentedRepository();
+            }
+            return repository;
+        }
+
+        @Override
+        public Set<NumberOfMoviesRented> getAll() {
+            return numberOfMoviesRentedDB;
+        }
+
+        @Override
+        public NumberOfMoviesRented create(NumberOfMoviesRented NumberOfMoviesRented) {
+            boolean success = numberOfMoviesRentedDB.add(NumberOfMoviesRented);
+            if (!success)
+                return null;
+            return NumberOfMoviesRented;
+        }
+
+        @Override
+        public NumberOfMoviesRented read(String movieID) {
+            for (NumberOfMoviesRented r : numberOfMoviesRentedDB)
+                if (r.getUserID().equals(userID))
+                    return r;
+            return null;
+        }
+
+        @Override
+        public NumberOfMoviesRented update(NumberOfMoviesRented numberOfMoviesRented) {
+            NumberOfMoviesRented oldNumberOfMoviesRented = read(numberOfMoviesRented.getUserID());
+            if (oldNumberOfMoviesRented != null) {
+                numberOfMoviesRentedDB.remove(oldNumberOfMoviesRented);
+                numberOfMoviesRentedDB.add(numberOfMoviesRented);
+                return numberOfMoviesRented;
+            }
+            return null;
+        }
+
+        @Override
+        public boolean delete(String movieID) {
+            NumberOfMoviesRented numberOfMoviesRentedToDelete = read(userID);
+            if (numberOfMoviesRentedToDelete == null)
+                return false;
+            numberOfMoviesRentedDB.remove(numberOfMoviesRentedToDelete);
+            return true;
+        }
+
+    }
---
+++ src/test/java/za/ac/cput/repository/NumberOfMoviesRentedRepositoryTest.java
@@ -0,0 +1,49 @@
+package za.ac.cput.repository;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import za.ac.cput.entity.NumberOfMoviesRented;
+import za.ac.cput.factory.NumberOfMoviesRentedFactory;
+import java.util.*;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class NumberOfMoviesRentedRepositoryTest {
+
+        private static NumberOfMoviesRentedRepository repository = NumberOfMoviesRentedRepository.getRepository();
+        private static NumberOfMoviesRented numberOfMoviesRented = NumberOfMoviesRentedFactory.createNumberOfMoviesRented("");
+
+        @Test
+        void a_create(){
+            NumberOfMoviesRented created = repository.create(numberOfMoviesRented);
+            assertEquals(created.getUserID(), numberOfMoviesRented.getUserID());
+            System.out.println("Create: " + created);
+        }
+
+        @Test
+        void b_read(){
+            NumberOfMoviesRented read = repository.read(NumberOfMoviesRented.getUserID());
+            assertNotNull(read);
+            System.out.println("Read" + read);
+        }
+
+        @Test
+        void c_update(){
+            NumberOfMoviesRented updated = new NumberOfMoviesRented.Builder().copy(numberOfMoviesRented).setUserID("").build();
+            assertNotNull(repository.update(updated));
+            System.out.println("Update: " + updated);
+        }
+
+        @Test
+        void e_delete(){
+            boolean success = repository.delete(numberOfMoviesRented.getUserID());
+            assertTrue(success);
+            System.out.println("Delete: "+ success);
+        }
+
+        @Test
+        void d_getAll(){
+            System.out.println("Show All: ");
+            System.out.println(repository.getAll());
+        }
+    }
