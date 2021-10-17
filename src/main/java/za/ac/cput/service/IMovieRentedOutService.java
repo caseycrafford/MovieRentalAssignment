9388bb21f3002d423fc661ca2ca52d8d16fb823f
lancekruger <lanceckruger2304@gmail.com>
2021-10-18 00:18:33 +0200

    Lance's Working Services and Controllers

---
+++ src/main/java/za/ac/cput/service/IMovieRentedOutService.java
@@ -0,0 +1,16 @@
+package za.ac.cput.service;
+
+import za.ac.cput.entity.MovieRentedOut;
+
+import java.util.Set;
+
+/**
+ * Author: Lance Kruger
+ * Student Number: 218159501
+ *
+ */
+
+public interface IMovieRentedOutService extends IService<MovieRentedOut, String>{
+    Set<MovieRentedOut> getAll();
+
+}
